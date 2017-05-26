2530b9a6982f5e64cf59a9b828f3b07e7fb78c14
Yeongseok Lee <sukgun28@naver.com>
2017-05-26 13:57:42 +0900

    New todo input event in javscript

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,6 +1,30 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	var addTodo = function(todo){
+		$('.todo-list').prepend(
+			'<li>\
+				<div class="view">\
+					<input class="toggle" type="checkbox">\
+					<label>'+ todo +'</label>\
+					<button class="destroy"></button>\
+				</div>\
+			</li>'
+		);
+	};
+
+	//Input enter key event on .new-todo
+	var $newTodo = $('.new-todo');
+	$newTodo.keyup(function(e){
+		//When enterKey
+		if (e.keyCode == 13){
+			var newTodo = $newTodo.val();
+			//Check input value isn't null
+			if(newTodo){
+				addTodo(newTodo);
+				$newTodo.val('');
+			}
+		}
+	});
 
 })(window);
