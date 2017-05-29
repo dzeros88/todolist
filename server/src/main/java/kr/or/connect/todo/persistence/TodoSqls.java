75d42580814e724737b2ac3240cf5ac4a9237256
Yeongseok Lee <sukgun28@naver.com>
2017-05-29 10:12:57 +0900

    Todo persistence

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -1,6 +1,13 @@
 package kr.or.connect.todo.persistence;
 
 public class TodoSqls {
+	static final String SELECT_ALL =
+			"SELECT id, todo, completed, date FROM todo";
+
+	static final String UPDATE =
+			"UPDATE todo SET completed = :completed WHERE id = :id";
+	
 	static final String DELETE_BY_ID =
 			"DELETE FROM todo WHERE id= :id";
+
 }
