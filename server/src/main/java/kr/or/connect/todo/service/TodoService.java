7b067b94c5e90350ee44c7ad8b4a00accba1c577
Yeongseok Lee <sukgun28@naver.com>
2017-05-29 10:12:02 +0900

    Todo service

--- server/src/main/java/kr/or/connect/todo/service/TodoService.java
+++ server/src/main/java/kr/or/connect/todo/service/TodoService.java
@@ -1,5 +1,35 @@
 package kr.or.connect.todo.service;
 
-public class TodoService {
+import java.util.Collection;
 
+import org.springframework.stereotype.Service;
+
+import kr.or.connect.todo.domain.Todo;
+import kr.or.connect.todo.persistence.TodoDao;
+
+@Service
+public class TodoService {
+	private TodoDao dao;
+	
+	public TodoService(TodoDao dao){
+		this.dao = dao;
+	}
+	
+	public Collection<Todo> findAll() {
+		return dao.selectAll();
+	}
+
+	public Integer create(Todo todo) {
+		return dao.insert(todo);
+	}
+
+	public boolean update(Integer id, Integer completed) {
+		int affected = dao.update(id, completed);
+		return affected == 1;
+	}
+
+	public boolean delete(Integer id) {
+		int affected = dao.deleteById(id);
+		return affected == 1;
+	}
 }
